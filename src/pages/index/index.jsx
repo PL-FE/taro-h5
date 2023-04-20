d817fbf38a4911f723c022880d79be449bc3de48
pengliang <41770773+PL-FE@users.noreply.github.com>
2023-04-20 05:45:32 +0000

    Update 3 files

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -1,32 +1,39 @@
-import { Component } from 'react'
-import { View, Text } from '@tarojs/components'
-import { AtButton } from 'taro-ui'
+import { Component } from "react";
+import { View, ScrollView } from "@tarojs/components";
+import { AtButton } from "taro-ui";
 
-import "taro-ui/dist/style/components/button.scss" // 按需引入
-import './index.css'
+import "taro-ui/dist/style/components/button.scss"; // 按需引入
+import "./index.css";
 
 export default class Index extends Component {
+  componentWillMount() {}
 
-  componentWillMount () { }
+  componentDidMount() {}
 
-  componentDidMount () { }
+  componentWillUnmount() {}
 
-  componentWillUnmount () { }
+  componentDidShow() {}
 
-  componentDidShow () { }
+  componentDidHide() {}
 
-  componentDidHide () { }
-
-  render () {
+  render() {
     return (
-      <View className='index'>
-        <Text>Hello world!</Text>
-        <AtButton type='primary'>I need Taro UI</AtButton>
-        <Text>Taro UI 支持 Vue 了吗？</Text>
-        <AtButton type='primary' circle={true}>支持</AtButton>
-        <Text>共建？</Text>
-        <AtButton type='secondary' circle={true}>来</AtButton>
+      <View className="index">
+        <ScrollView
+          className="body"
+          scrollY
+          scrollWithAnimation
+          scrollTop={230}
+        >
+          {new Array(200).fill(1).map((a, i) => {
+            return (
+              <View key={i} className="item">
+                {i}
+              </View>
+            );
+          })}
+        </ScrollView>
       </View>
-    )
+    );
   }
 }
